dab62cf38d6c2d899ca4f45d6f3101969f80a2e5
Tarekjalali <jlali.tare9@gmail.com>
2024-12-19 13:46:16 +0100

    use .env

--- src/Redux/Actions/AuthActions.js
+++ src/Redux/Actions/AuthActions.js
@@ -1,182 +1,131 @@
-import axios from 'axios'
-import { CURRENTUSER, FAIL, GETUSERS, LOGIN, LOGOUT, REGISTER, } from '../ActionTypes/AuthTypes'
+import axios from 'axios';
+import { 
+  CURRENTUSER, 
+  FAIL, 
+  GETUSERS, 
+  LOGIN, 
+  LOGOUT, 
+  REGISTER, 
+} from '../ActionTypes/AuthTypes';
 
+// Base URL from the environment variable
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-export const register=(user,navigate)=>async(dispatch)=>{
+export const register = (user, navigate) => async (dispatch) => {
+  try {
+    const res = await axios.post(`${API_BASE_URL}/api/users/Register`, user);
+    dispatch({
+      type: REGISTER,
+      payload: res.data,
+    });
+    navigate('/Profil');
+  } catch (error) {
+    dispatch({
+      type: FAIL,
+      payload: error.response.data.errors,
+    });
+  }
+};
 
-    try {
+export const login = (user, navigate) => async (dispatch) => {
+  try {
+    const res = await axios.post(`${API_BASE_URL}/api/users/SignIn`, user);
+    dispatch({
+      type: LOGIN,
+      payload: res.data,
+    });
+    navigate('/Profil');
+  } catch (error) {
+    dispatch({
+      type: FAIL,
+      payload: error.response.data.errors,
+    });
+  }
+};
 
-        const res = await axios.post('/api/users/Register',user)
+export const logout = () => {
+  return {
+    type: LOGOUT,
+  };
+};
 
-        dispatch(
-            {
-                type : REGISTER,
-                payload:res.data
-            }
-        )
+export const currentuser = () => async (dispatch) => {
+  try {
+    const config = { headers: { authorized: localStorage.getItem('token') } };
+    const res = await axios.get(`${API_BASE_URL}/api/users/currentUser`, config);
+    dispatch({
+      type: CURRENTUSER,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: FAIL,
+      payload: error.response.data.errors,
+    });
+  }
+};
 
-        navigate('/Profil')
-        
+export const deleteAccount = (id, navigate) => async (dispatch) => {
+  try {
+    await axios.delete(`${API_BASE_URL}/api/users/deleteProfile/${id}`);
+    dispatch({
+      type: LOGOUT,
+    });
+    navigate('/');
+  } catch (error) {
+    dispatch({
+      type: FAIL,
+      payload: error.response.data.errors,
+    });
+  }
+};
 
+export const updateuser = (id, changes, navigate) => async (dispatch) => {
+  try {
+    const formData = new FormData();
+    formData.append('image', changes.profilePic);
 
-        
-    } catch (error) {
-        dispatch(
-            {
-                type : FAIL,
-                payload : error.response.data.errors
-            }
-        )
-    }
+    const imgUploadRes = await axios.post(
+      'https://api.imgbb.com/1/upload?key=22970270ad4334a985e23993195591f8',
+      formData
+    );
 
-}
+    const profilePic = imgUploadRes.data.data.url;
 
-export const login=(user, navigate)=>async(dispatch)=>{
+    await axios.put(`${API_BASE_URL}/api/users/UpdateProfil/${id}`, { ...changes, profilePic });
+    dispatch(currentuser());
+    navigate('/Profil');
+  } catch (error) {
+    dispatch({
+      type: FAIL,
+      payload: error.response.data.errors,
+    });
+  }
+};
 
-    try {
+export const getusers = () => async (dispatch) => {
+  try {
+    const res = await axios.get(`${API_BASE_URL}/api/users/getAllUsers`);
+    dispatch({
+      type: GETUSERS,
+      payload: res.data.users,
+    });
+  } catch (error) {
+    dispatch({
+      type: FAIL,
+      payload: error.response.data.errors,
+    });
+  }
+};
 
-        const res = await axios.post('/api/users/SignIn',user)
-
-        dispatch(
-            {
-                type : LOGIN,
-                payload : res.data
-            }
-        )
-        navigate('/Profil')
-        
-    } catch (error) {
-        dispatch(
-            {
-                type : FAIL,
-                payload : error.response.data.errors
-            }
-        )
-    }
-
-}
-
-export const logout=()=>{
-   return(
-    {
-        type : LOGOUT
-    }
-   )
-}
-
-export const currentuser =()=>async(dispatch)=>{
-
-    try {
-
-        const config = {headers : { authorized : localStorage.getItem('token')}}
-
-        const res = await axios.get('/api/users/currentUser',config)
-
-        dispatch(
-            {
-                type : CURRENTUSER,
-                payload : res.data
-            }
-        )
-        
-    } catch (error) {
-        dispatch(
-            {
-                type : FAIL,
-                payload : error.response.data.errors
-            }
-        )
-    }
-
-
-}
-
-export const deleteAccount=(id,navigate)=>async(dispatch)=>{
-
-    try {
-
-        await axios.delete(`/api/users/deleteProfile/${id}`)
-        dispatch(
-            {
-                type : LOGOUT
-            }
-        )
-        navigate('/')
-        
-    } catch (error) {
-        dispatch(
-            {
-                type : FAIL,
-                payload : error.response.data.errors
-            }
-        )
-    }
-
-}
-
-export const updateuser=(id,changes,navigate)=>async(dispatch)=>{
-
-    try {
-        
-        const formData = new FormData()
-        formData.append('image' , changes.profilePic)
-        const res = await axios.post('https://api.imgbb.com/1/upload?key=22970270ad4334a985e23993195591f8', formData)
-        
-        const profilePic = res.data.data.url
-        await axios.put(`/api/users/UpdateProfil/${id}`,{...changes, profilePic})
-        dispatch(currentuser())
-        navigate('/Profil')
-
-
-    } catch (error) {
-
-        dispatch(
-            {
-                type : FAIL,
-                payload : error.response.data.errors
-            }
-        )
-        
-    }
-
-}
-
-export const getusers = ()=>async(dispatch)=>{
-
-    try {
-        const res = await axios.get('/api/users/getAllUsers')
-        dispatch(
-            {
-                type : GETUSERS,
-                payload : res.data.users
-            }
-        )
-    } catch (error) {
-        dispatch(
-            {
-                type : FAIL,
-                payload : error.response.data.errors
-            }
-        )
-    }
-
-}
-
-export const deleteUsers=(id)=>async(dispatch)=>{
-
-    try {
-        await axios.delete(`/api/users/deleteProfile/${id}`)
-        dispatch(            
-                getusers()            
-        )
-        
-    } catch (error) {
-        dispatch(
-            {
-                type : FAIL,
-                payload : error.response.data.errors
-            }
-        )
-    }
-
-}
+export const deleteUsers = (id) => async (dispatch) => {
+  try {
+    await axios.delete(`${API_BASE_URL}/api/users/deleteProfile/${id}`);
+    dispatch(getusers());
+  } catch (error) {
+    dispatch({
+      type: FAIL,
+      payload: error.response.data.errors,
+    });
+  }
+};
--- src/Redux/Actions/EventsActions.js
+++ src/Redux/Actions/EventsActions.js
@@ -1,125 +1,86 @@
-import axios from 'axios'
-import { GETEVENTS, GETMYEVENTS, GETONEEVENT } from '../ActionTypes/EventTypes'
+import axios from 'axios';
+import { GETEVENTS, GETMYEVENTS, GETONEEVENT } from '../ActionTypes/EventTypes';
 
+// Base URL from the environment variable
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-export const getevents =(req,res)=>async(dispatch)=>{
+export const getevents = () => async (dispatch) => {
+  try {
+    const res = await axios.get(`${API_BASE_URL}/api/events/getAllEvents`);
+    dispatch({
+      type: GETEVENTS,
+      payload: res.data.events,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    try {
-        const res = await axios.get('/api/events/getAllEvents')
+export const addevent = (event, navigate) => async (dispatch) => {
+  try {
+    const formData = new FormData();
+    formData.append('image', event.eventPic);
 
-        dispatch(
-            {
-                type : GETEVENTS,
-                payload : res.data.events
-            }
-        )
-    } catch (error) {
-        console.log(error)
+    const resAPI = await axios.post(
+      'https://api.imgbb.com/1/upload?key=22970270ad4334a985e23993195591f8',
+      formData
+    );
+
+    const eventPic = resAPI.data.data.url;
+    await axios.post(`${API_BASE_URL}/api/events/createEvent`, { ...event, eventPic });
+
+    dispatch(getevents());
+    navigate('/Events');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getonevent = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`${API_BASE_URL}/api/events/getOneEvent/${id}`);
+    dispatch({
+      type: GETONEEVENT,
+      payload: res.data,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const deleteEvent = (id, location, navigate, userId) => async (dispatch) => {
+  try {
+    await axios.delete(`${API_BASE_URL}/api/events/deleteEvent/${id}`);
+
+    if (location.pathname !== '/Profil') {
+      dispatch(getevents());
+      navigate('/Events');
+    } else {
+      dispatch(getMyEvents(userId));
     }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-}
+export const updateEvent = (id, changes, navigate) => async (dispatch) => {
+  try {
+    await axios.put(`${API_BASE_URL}/api/events/updateEvent/${id}`, changes);
+    dispatch(getevents());
+    navigate('/Events');
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const addevent =(event, navigate)=>async(dispatch)=>{
-
-    try {
-        const formData = new FormData()
-        formData.append('image' , event.eventPic)
-        const resAPI = await axios.post('https://api.imgbb.com/1/upload?key=22970270ad4334a985e23993195591f8', formData)
-        const eventPic = resAPI.data.data.url
-        const res = await axios.post('/api/events/createEvent', {...event, eventPic})
-
-        dispatch(
-         getevents()
-            
-        )
-        navigate('/Events')
-        
-    } catch (error) {
-        console.log(error)
-    }
-
-}
-
-export const getonevent =(id)=>async(dispatch)=>{
-
-
-
-    try {
-        const res = await axios.get(`/api/events/getOneEvent/${id}`)
-
-        dispatch(
-            {
-                type : GETONEEVENT,
-                payload : res.data
-            }
-        )
-                
-    } catch (error) {
-        console.log(error)
-    }
-
-}
-
-export const deleteEvent=(id,location,navigate,userId)=>async(dispatch)=>{
-
-    try {
-
-        await axios.delete(`/api/events/deleteEvent/${id}`)
-        
-        
-
-
-        if (location.pathname !== '/Profil') {
-            dispatch(
-                getevents()
-            )
-            navigate('/Events')
-        } else{
-            dispatch(
-                getMyEvents(userId)
-            )
-        }
-        
-    } catch (error) {
-        console.log(error)
-    }
-
-}
-
-export const updateEvent=(id,changes,navigate)=>async(dispatch)=>{
-
-    try {
-
-        await axios.put(`/api/events/updateEvent/${id}`,changes)
-
-        dispatch(getevents())
-
-        navigate('/Events')
-        
-    } catch (error) {
-        console.log(error)
-    }
-
-}
-
-
-export const getMyEvents =(id)=>async(dispatch)=>{
-
-    try {
-        
-        const res = await axios.get(`/api/events/getMyEvents/${id}`)
-
-        
-
-        dispatch(
-            {
-                type : GETMYEVENTS,
-                payload : res.data
-            }
-        )
-        
-    } catch (error) {
-        console.log(error)
-    }
-
-}
+export const getMyEvents = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`${API_BASE_URL}/api/events/getMyEvents/${id}`);
+    dispatch({
+      type: GETMYEVENTS,
+      payload: res.data,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
